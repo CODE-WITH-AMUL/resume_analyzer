2ee0326c11397812bfb77fc7e78c9ad92f545d68
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2025-12-09 12:21:26 +0545

    Api config

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  REACT_BASE_URL: 'http://127.0.0.1:8000', // For web
+  REACT_BASE_URL_ANDROID: 'http://10.0.2.2:8000', // For Android emulator
+  REACT_BASE_URL_IOS: 'http://localhost:8000', // For iOS simulator
+};
+export default CONFIG;
